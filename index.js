4ff7c69319b665b1fc9ee8cebaa1393eae13b8ae
Mohammed Mahdi <mak.zero.x@gmail.com>
2017-09-18 22:44:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,17 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
     return "I can\'t hear you!";
 
-    else
+    else if
 
       (string.toUpperCase() === string)
       return "YES INDEED!";
+
+      else 
+      
+      (string === ("I love you, Grandma."))
+      return "I love you, too."
+        
+      
 
 
 }
